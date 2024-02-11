1a1a7b96e0c92fc5d540cabb337345276680e4b8
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-11 18:06:31 +0430

    Style: User Input

--- 03 - GitHub users/assets/js/index.js
+++ 03 - GitHub users/assets/js/index.js
@@ -1,1 +1,14 @@
 // const URL = "https://api.github.com/users/";
+const userName = document.querySelector("#username");
+const userImage = document.querySelector("#user-image");
+const userFollowers = document.querySelector("#followers");
+const userFollowing = document.querySelector("#following");
+const userRepositories = document.querySelector("#repos");
+const name = document.querySelector(".user-name");
+const bio = document.querySelector(".user-bio");
+const location = document.querySelector(".user-location");
+const stars = document.querySelector(".user-stars");
+const respositoriesList = document.querySelector("#repos-list");
+
+const userInput = document.querySelector("#user-input");
+const findBtn = document.querySelector("#find-btn");
